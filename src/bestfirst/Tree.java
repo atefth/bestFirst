21bca4421c8bd0eaa4088b6efa66ae160c9e161a
Atef Haque <atefth@gmail.com>
2013-06-11 00:01:35 +0600

    Fixed the test Tree which is used to test the algorithm

--- src/bestfirst/Tree.java
+++ src/bestfirst/Tree.java
@@ -26,40 +26,52 @@ class Tree {
         Node firstNode = new Node(3, 2);
         Node secondNode = new Node(1, 1);
         Node thirdNode = new Node(4, 3);
+        Node fourthNode = new Node(5, 1);
+        
         Node first1Node = new Node(1, 1);
         Node first2Node = new Node(3, 2);
-        Node second1Node = new Node(1, 1);        
-        Node third1Node = new Node(4, 3);
-        Node third2Node = new Node(1, 4);
-        Node third3Node = new Node(6, 1);
-        Node first1Node1 = new Node(5, 2);
         
-        Tree.root.append(third3Node);
+        Node second1Node = new Node(1, 1);  
+        
+        Node third1Node = new Node(4, 3);
+        Node third2Node = new Node(3, 4);
+        Node third3Node = new Node(2, 1);
+        
+        Tree.root.append(firstNode);
         Tree.root.append(secondNode);
-        Tree.root.append(second1Node);
-        Tree.root.append(first1Node);
-        firstNode.append(third2Node);
+        Tree.root.append(thirdNode);
+        Tree.root.append(fourthNode);
+        
+        firstNode.append(first1Node);
         firstNode.append(first2Node);
-        secondNode.append(first1Node1);
+        
+        secondNode.append(second1Node);
+        
         thirdNode.append(third1Node);
-        thirdNode.append(first1Node);
-        thirdNode.append(thirdNode);
-        first1Node.append(firstNode);
-        first2Node.append(first1Node1);
-        first2Node.append(second1Node);
-        second1Node.append(first2Node);
-        third1Node.append(Tree.root);
-        third1Node.append(third3Node);
-        third1Node.append(first1Node1);
+        thirdNode.append(third2Node);
+        thirdNode.append(third3Node);
+        
+        fourthNode.append(second1Node);
+        
+        first1Node.append(third2Node);
+        
+        first2Node.append(secondNode);
+        first2Node.append(third1Node);
+        
+        second1Node.append(first1Node);
+        
+        third1Node.append(fourthNode);
+        third1Node.append(second1Node);
+        third1Node.append(firstNode);
+        
         third2Node.append(first1Node);
         third2Node.append(firstNode);
         third2Node.append(third1Node);
-        third2Node.append(thirdNode);
-        third3Node.append(secondNode);
-        first1Node1.append(second1Node);
-        first1Node1.append(third2Node);
+        third2Node.append(third2Node);
         
-        Tree.target = first1Node1;
+        third3Node.append(fourthNode);
+        
+        Tree.target = third2Node;
     }
     
 }
